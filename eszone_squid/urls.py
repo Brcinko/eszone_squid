8abf14e663c3bab1b345370937eaa2544758d6ae
Brcinko <lukas.klescinec@gmail.com>
2015-02-06 16:38:28 +0100

    Added new views

--- eszone_squid/urls.py
+++ eszone_squid/urls.py
@@ -6,7 +6,7 @@ from api_squid import views
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
                        url(r'^admin/', include(admin.site.urls)),
-                       url(r'^acl_rules/(?P<aclrules_id>\d+)/$', views.acl_rule, name='acl_rule'),
-                       # url(r'^acl_rules/[0-9]{1,}/$', views.acl_rule, name='acl_rule'),
-                       url(r'^acl_rules/$', views.AclRulesViewSet, name='AclRulesViewSet'),
+                       url(r'^acl_rules/(?P<aclrules_id>\d+)/$', views.acl_rule_detail, name='acl_rule_detail'),
+                       url(r'^acl_rules/$', views.acl_rules_list, name='acl_rules_list'),
                        )
+
