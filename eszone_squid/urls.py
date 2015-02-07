f56d08a217d2f8379ccc9981452ff468b9716a4d
Brcinko <lukas.klescinec@gmail.com>
2015-02-07 16:32:30 +0100

    acl_list

--- eszone_squid/urls.py
+++ eszone_squid/urls.py
@@ -6,7 +6,9 @@ from api_squid import views
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
                        url(r'^admin/', include(admin.site.urls)),
-                       url(r'^acl_rules/(?P<aclrules_id>\d+)/$', views.acl_rule_detail, name='acl_rule_detail'),
+                       url(r'^acl_rules/(?P<pk>\d+)/$', views.acl_rule_detail, name='acl_rule_detail'),
                        url(r'^acl_rules/$', views.acl_rules_list, name='acl_rules_list'),
+                       # http_access
+                       # url(r'^acl_list/$', views.acl_list, name='acl_list'),
                        )
 
