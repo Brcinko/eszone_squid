63302c97bf09e8d69a406d640a933650079567b5
Brcinko <lukas.klescinec@gmail.com>
2015-02-10 15:01:49 +0100

    new model acl-list

--- eszone_squid/urls.py
+++ eszone_squid/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
                        url(r'^acl_rules/(?P<pk>\d+)/$', views.acl_rule_detail, name='acl_rule_detail'),
                        url(r'^acl_rules/$', views.acl_rules_list, name='acl_rules_list'),
                        # http_access
-                       # url(r'^acl_list/$', views.acl_list, name='acl_list'),
+                       url(r'^acl_list/$', views.acl_list, name='acl_list'),
+                       url(r'^acl_list/(?P<pk>\d+)/$', views.acl_list_detail, name='acl_list_detail'),
                        )
 
