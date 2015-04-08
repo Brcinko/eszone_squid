a7a1f092a51f313a79da656b91ae925dd456ec59
Brcinko <lukas.klescinec@gmail.com>
2015-04-08 20:47:42 +0200

    new file api_squid.urls, new url format /api_squid/something

--- eszone_squid/urls.py
+++ eszone_squid/urls.py
@@ -5,19 +5,6 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from api_squid import views
 
 urlpatterns = patterns('',
-                       url(r'^$', views.index, name='index'),
-                       url(r'^admin/', include(admin.site.urls)),
-                       url(r'^acl_rules/(?P<pk>\d+)/$', views.acl_rule_detail, name='acl_rule_detail'),
-                       url(r'^acl_rules/$', views.acl_rules_list, name='acl_rules_list'),
-                       # http_access
-                       url(r'^acl_list/$', views.acl_list, name='acl_list'),
-                       url(r'^acl_list/(?P<pk>\d+)/$', views.acl_list_detail, name='acl_list_detail'),
-                       # authentication
-                       url(r'^authentication/$', views.authentication, name='authentication'),
-                       url(r'^authentication_db/$', views.authentication_db, name='authentication_db'),
-                       url(r'^authentication/(?P<pk>\d+)/$', views.authentication_detail, name='authentication_detail'),
-                       url(r'^authentication_db/(?P<pk>\d+)/$', views.authentication_db_detail, name='authentication_db_detail'),
-                       # reconfigure
-                       url(r'^update_config/$', views.update_config, name='update_config'),
+                       url(r'^api_squid/', include('api_squid.urls'))
                        )
-urlpatterns = format_suffix_patterns(urlpatterns)
+
